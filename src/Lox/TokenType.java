2b3f50198e52edaf780dc62541c0cf5654d70e6d
Ayush Kumar <ayushrocks456@gmail.com>
2024-07-17 16:01:42 +0530

    First Time Setup

---
+++ src/Lox/TokenType.java
@@ -0,0 +1,21 @@
+package Lox;
+
+public enum TokenType{
+    LEFT_PARENTHESIS, RIGHT_PARENTHESIS, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    //literals
+    IDENTIFIER, STRING, NUMBER,
+
+    //keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
